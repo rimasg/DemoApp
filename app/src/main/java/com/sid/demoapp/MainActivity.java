d0a3432794d611f3e7215572f2dc188b9641097f
Rimas G <rimasg@gmail.com>
2016-03-15 08:26:04 +0200

    Updates

--- app/src/main/java/com/sid/demoapp/MainActivity.java
+++ app/src/main/java/com/sid/demoapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.sid.demoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.concurrent.Callable;
+
+import bolts.Continuation;
+import bolts.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnBoltsTask = (Button) findViewById(R.id.btnBoltsTask);
+        btnBoltsTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runBoltsTask();
+            }
+        });
+    }
+
+    public void runBoltsTask() {
+        Task.callInBackground(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                final String strToReturn = "I was call by BoltsTask";
+                return strToReturn;
+            }
+        }).onSuccess(new Continuation<String, Void>() {
+            @Override
+            public Void then(Task<String> task) throws Exception {
+                System.out.println(task.getResult());
+                return null;
+            }
+        });
     }
 }
