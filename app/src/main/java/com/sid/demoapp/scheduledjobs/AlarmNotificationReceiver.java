f9eba0bff0a77e1d17e8782bc9ae5d750e4b192e
Rimas G <rimasg@gmail.com>
2016-09-04 11:29:27 +0300

    ScheduleAlarmManager added

---
+++ app/src/main/java/com/sid/demoapp/scheduledjobs/AlarmNotificationReceiver.java
@@ -0,0 +1,33 @@
+package com.sid.demoapp.scheduledjobs;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.sid.demoapp.MainActivity;
+
+/**
+ * Created by Okis on 2016.09.04 @ 10:42.
+ */
+public class AlarmNotificationReceiver extends BroadcastReceiver{
+    private static final int NOTIFICATION_ID = 966;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final Intent notificationIntent = new Intent(context, MainActivity.class);
+        final PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        final Notification.Builder notificationBuilder = new Notification.Builder(context)
+                .setTicker("DemoApp started")
+                .setSmallIcon(android.R.drawable.ic_notification_overlay)
+                .setContentTitle("DemoApp")
+                .setContentText("Go ahead and run DemoApp")
+                .setContentIntent(contentIntent);
+
+        final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
+}
---
+++ app/src/main/java/com/sid/demoapp/scheduledjobs/ScheduleAlarmManager.java
@@ -0,0 +1,33 @@
+package com.sid.demoapp.scheduledjobs;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Okis on 2016.09.04 @ 09:53.
+ */
+
+public final class ScheduleAlarmManager {
+    private static AlarmManager manager = null;
+    private Activity activity;
+
+    public ScheduleAlarmManager(Activity activity) {
+        this.activity = activity;
+        if (manager == null) {
+            manager = (AlarmManager) activity.getSystemService(Context.ALARM_SERVICE);
+        }
+    }
+
+    public void scheduleAlarmAfter(int seconds) {
+        PendingIntent operation = buildNotificationReceiverIntent();
+        manager.set(AlarmManager.RTC, System.currentTimeMillis() + seconds * 1000, operation);
+    }
+
+    private PendingIntent buildNotificationReceiverIntent() {
+        final Intent notificationReceiverIntent = new Intent(activity, AlarmNotificationReceiver.class);
+        return PendingIntent.getBroadcast(activity, 0, notificationReceiverIntent, 0);
+    }
+}
