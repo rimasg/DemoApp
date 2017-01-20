4eb9f9a819b9d43ebbb23a66bf22b32080fbd535
Rimas Gaina <rimasg@gmail.com>
2017-01-20 06:59:07 +0200

    Update OtherFragment.java

--- app/src/main/java/com/sid/demoapp/OtherFragment.java
+++ app/src/main/java/com/sid/demoapp/OtherFragment.java
@@ -65,10 +65,6 @@ public class OtherFragment extends Fragment {
         }
     };
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public OtherFragment() {
     }
 
