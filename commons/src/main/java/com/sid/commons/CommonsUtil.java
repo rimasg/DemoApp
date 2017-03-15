c55089ea8c03093116c723c19d25a86ee7681a3f
Rimas G <rimasg@gmail.com>
2017-03-15 07:24:47 +0200

    Commons Library added

---
+++ commons/src/main/java/com/sid/commons/CommonsUtil.java
@@ -0,0 +1,11 @@
+package com.sid.commons;
+
+/**
+ * Created by Okis on 2017.03.15.
+ */
+
+public class CommonsUtil {
+    public static void print(String s) {
+        System.out.println("The value is: " + s);
+    }
+}
