43176be8ec9ec5591b80a97ff80ffa81bec03c69
Rimas G <rimasg@gmail.com>
2018-06-09 13:26:41 +0300

    Minor fixes

--- app/src/main/java/com/sid/demoapp/lifecycleobserver/TestLifecycleObserver.java
+++ app/src/main/java/com/sid/demoapp/lifecycleobserver/TestLifecycleObserver.java
@@ -1,35 +1,40 @@
 package com.sid.demoapp.lifecycleobserver;
 
-import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleObserver;
 import android.arch.lifecycle.OnLifecycleEvent;
+
+import static android.arch.lifecycle.Lifecycle.Event.ON_CREATE;
+import static android.arch.lifecycle.Lifecycle.Event.ON_PAUSE;
+import static android.arch.lifecycle.Lifecycle.Event.ON_RESUME;
+import static android.arch.lifecycle.Lifecycle.Event.ON_START;
+import static android.arch.lifecycle.Lifecycle.Event.ON_STOP;
 
 /**
  * Created by rgaina on 09/06/2018.
  */
 public class TestLifecycleObserver implements LifecycleObserver {
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    @OnLifecycleEvent(ON_CREATE)
     public void onCreate() {
         printLifeCycleStatus("onCreate");
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    @OnLifecycleEvent(ON_START)
     public void onStart() {
         printLifeCycleStatus("onStart");
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    @OnLifecycleEvent(ON_RESUME)
     public void onResume() {
         printLifeCycleStatus("onResume");
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    @OnLifecycleEvent(ON_PAUSE)
     public void onPause() {
         printLifeCycleStatus("onPause");
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    @OnLifecycleEvent(ON_STOP)
     public void onStop() {
         printLifeCycleStatus("onStop");
     }
