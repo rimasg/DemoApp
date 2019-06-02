1677ab41da60220f2d45d5432b4d5ae4e3abd4e4
Rimas G <rimasg@gmail.com>
2019-06-02 16:12:34 +0300

    Added Timber to Gradle

--- app/src/main/java/com/sid/demoapp/MainApplication.kt
+++ app/src/main/java/com/sid/demoapp/MainApplication.kt
@@ -1,13 +1,25 @@
 package com.sid.demoapp
 
-import com.sid.demoapp.di.DaggerAppComponent
+import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import timber.log.Timber
 
 /**
  * Created by rgaina on 06/05/2018.
  */
 class MainApplication : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        // ThreeTenBP for times and dates
+        AndroidThreeTen.init(this)
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().create(this)
