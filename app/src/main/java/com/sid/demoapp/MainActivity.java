f98c995cdaebfbe566786317bc0d92cf5eed1b00
Rimas G <rimasg@gmail.com>
2022-05-12 15:57:11 +0300

    feat: migrated the existing splash screen to Android 12+

--- app/src/main/java/com/sid/demoapp/MainActivity.java
+++ app/src/main/java/com/sid/demoapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+        final SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main_transparent_toolbar);
         setContentView(R.layout.activity_main_new);
